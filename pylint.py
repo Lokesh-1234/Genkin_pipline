63322a755a4d2e55fe5a0243b28279e2a2a8ec74
Lokesh-1234 <104615534+Lokesh-1234@users.noreply.github.com>
2022-05-08 20:08:31 +0530

    Update pylint.py

--- pylint.py
+++ pylint.py
@@ -2,18 +2,18 @@
 import string
 
 SHIFT = 3
-CHOICE = input("would you like to ENCODE or DECODE")
+CHOICE = input("would you like to encode or decode")
 WORD = input("Please enter text")
 LETTERS = string.ascii_letters + string.punctuation + string.digits
 ENCODED = ''
-if CHOICE == "ENCODE":
+if CHOICE == "encode":
     for letter in WORD:
         if letter == ' ':
             ENCODED = ENCODED + ' '
         else:
             x = LETTERS.index(letter) + SHIFT
             ENCODED = ENCODED + LETTERS[x]
-if CHOICE == "DECODE":
+if CHOICE == "decode":
     for letter in WORD:
         if letter == ' ':
             ENCODED = ENCODED + ' '
